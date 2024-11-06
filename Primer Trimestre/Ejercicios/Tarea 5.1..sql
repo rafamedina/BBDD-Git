f155c0c91ae4d49abdc09d0f8ac0bd7b29c7c9bb
rafamedina <rafael.medinaayuso24@campusfp.es>
2024-11-06 10:26:54 +0100

    5.1

--- Primer Trimestre/Ejercicios/Tarea 5.1..sql
+++ Primer Trimestre/Ejercicios/Tarea 5.1..sql
@@ -1,4 +1,5 @@
 -- Crear la base de datos
+Drop database if exists ejercicios_crud; 
 CREATE DATABASE IF NOT EXISTS ejercicios_crud;
 USE ejercicios_crud;
 
@@ -71,32 +72,50 @@ insert ignore clientes(nombre, email, telefono, puntos, fecha_registro) values
 
 -- Update: ¿Cómo se cambia el nombre de un cliente con email específico a 'Cliente Actualizado'?
 
-
+update clientes set nombre = 'cliente Actualizado' where email = 'carlos@mail.com';
 
 -- Delete: ¿Cómo se eliminan todos los clientes que tienen menos de 50 puntos?
 
+delete from clientes where puntos < 50;
 
 -- Update: ¿Cómo se actualiza el telefono de todos los clientes para agregar un prefijo '34-' al inicio de cada número de teléfono?
 
+update clientes set telefono = concat('34-', telefono) where telefono is not null;
 
 -- Insert: ¿Cómo se insertan varios clientes en una sola consulta?
+
+INSERT INTO clientes (nombre, email, telefono, puntos, fecha_registro)
+VALUES
+('Iker', 'ikeracv28@mail.com', '968458452', 150, '2023-06-28'),
+('Alex', 'Alexword@mail.com', '189155174', 13, '2023-10-30');
 
 
 -- Update: ¿Cómo se reinicia a cero el campo puntos para todos los clientes cuyo nombre empiece con 'A'?
 
+update clientes set puntos= '0' where nombre like 'a%';
 
 -- Insert: ¿Cómo se inserta un cliente utilizando un subquery para calcular su puntos inicial basado en el promedio de todos los puntos?
 
+INSERT INTO clientes (nombre, email, telefono, puntos, fecha_registro)
+VALUES
+('Marcos', 'Warkoz@mail.com', '89746521', (SELECT avg(puntos)), '2022-06-28');
+
 -- Delete: ¿Cómo se eliminan todos los clientes registrados hace más de 5 años usando un campo fecha_registro?
 
+--                       delete from clientes where fecha_registro (no tengo ni idea)
 
 -- Update: ¿Cómo se incrementan los puntos de todos los clientes con más de 100 puntos y se actualiza la fecha de modificación a la fecha actual?
 
+update clientes set puntos = '150' and fecha_registro = '2024-11-06' where puntos > '100';
 
 -- Insert: ¿Cómo se inserta un cliente y se devuelve el ID generado utilizando la función LAST_INSERT_ID()?
 
 
 -- Update: ¿Cómo se actualizan los puntos de todos los clientes a la mitad de su valor actual?
 
+update clientes set puntos = (puntos*0.5);
 
 -- Delete: ¿Cómo se eliminan todos los clientes cuyo telefono comienza con el prefijo '123-'?
+
+delete from clientes where telefono like '123%';
+
